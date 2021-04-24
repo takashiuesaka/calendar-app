612887607b91ac3b32e8d388360f253cbf4d8e6f
Takashi Uesaka <takashiuesaka1224@gmail.com>
2021-04-24 20:56:04 +0900

    Add Calendar component

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Calendar = () => {
+    return <><p>Hello World</p></>
+};
+
+export default Calendar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Calendar from './components/Calendar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calendar />
   </React.StrictMode>,
   document.getElementById('root')
 );
