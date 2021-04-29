f91cd509b52b9f0b42d43c3e100308ec37e0f431
Takashi Uesaka <takashiuesaka1224@gmail.com>
2021-04-29 21:01:35 +0900

    イベントをカレンダーにとりあえず表示

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -6,7 +6,7 @@ const Calendar = () => {
 
     const [currentDate, setCurrentDate] = useState(moment());
     const [currentMonth, setCurrentMonth] = useState<string>();
-    const [calendars, setCalendars] = useState<{ date: number, month: string }[][]>([]);
+    const [calendars, setCalendars] = useState<{ date: number, month: string, dayEvents: { id: number, name: string, start: string, end: string, color: string }[] }[][]>([]);
 
     const getStartDate = (date: moment.Moment) => {
         const startDate = moment(date).startOf("month");
@@ -34,9 +34,11 @@ const Calendar = () => {
         for (let week = 0; week < weekNumber; week++) {
             let weekRow = [];
             for (let day = 0; day < 7; day++) {
+                let dayEvents = getDayEvents(calendarDate);
                 weekRow.push({
                     date: calendarDate.get("date"),
-                    month: calendarDate.format('YYYY-MM')
+                    month: calendarDate.format('YYYY-MM'),
+                    dayEvents
                 });
 
                 calendarDate.add(1, "days");
@@ -73,6 +75,40 @@ const Calendar = () => {
         );
     }
 
+    const events: { id: number, name: string, start: string, end: string, color: string }[] = [
+        { id: 1, name: "ミーティング", start: "2021-01-01", end: "2021-01-01", color: "blue" },
+        { id: 2, name: "イベント", start: "2021-01-02", end: "2021-01-03", color: "limegreen" },
+        { id: 3, name: "会議", start: "2021-01-06", end: "2021-01-06", color: "deepskyblue" },
+        { id: 4, name: "有給", start: "2021-01-08", end: "2021-01-08", color: "dimgray" },
+        { id: 5, name: "海外旅行", start: "2021-01-08", end: "2021-01-11", color: "navy" },
+        { id: 6, name: "誕生日", start: "2021-01-16", end: "2021-01-16", color: "orange" },
+        { id: 7, name: "イベント", start: "2021-01-12", end: "2021-01-15", color: "limegreen" },
+        { id: 8, name: "出張", start: "2021-01-12", end: "2021-01-13", color: "teal" },
+        { id: 9, name: "客先訪問", start: "2021-01-14", end: "2021-01-14", color: "red" },
+        { id: 10, name: "パーティ", start: "2021-01-15", end: "2021-01-15", color: "royalblue" },
+        { id: 12, name: "ミーティング", start: "2021-01-18", end: "2021-01-19", color: "blue" },
+        { id: 13, name: "イベント", start: "2021-01-21", end: "2021-01-21", color: "limegreen" },
+        { id: 14, name: "有給", start: "2021-01-20", end: "2021-01-20", color: "dimgray" },
+        { id: 15, name: "イベント", start: "2021-01-25", end: "2021-01-28", color: "limegreen" },
+        { id: 16, name: "会議", start: "2021-01-21", end: "2021-01-21", color: "deepskyblue" },
+        { id: 17, name: "旅行", start: "2021-01-23", end: "2021-01-24", color: "navy" },
+        { id: 18, name: "ミーティング", start: "2021-01-28", end: "2021-01-28", color: "blue" },
+        { id: 19, name: "会議", start: "2021-01-12", end: "2021-01-12", color: "deepskyblue" },
+        { id: 20, name: "誕生日", start: "2021-01-30", end: "2021-01-30", color: "orange" },
+    ];
+
+    const getDayEvents = (date: moment.Moment) => {
+        return events.filter(event => {
+            let startDate = moment(event.start).format('YYYY-MM-DD')
+            let endDate = moment(event.end).format('YYYY-MM-DD')
+            let Date = date.format('YYYY-MM-DD')
+            if (startDate <= Date && endDate >= Date)
+                return true;
+
+            return false;
+        });
+    };
+
     useEffect(() => {
         setCalendars(getCalendar(currentDate));
         setCurrentMonth(currentDate.format('YYYY-MM'));
@@ -98,6 +134,17 @@ const Calendar = () => {
                                                 <div className='calendar-day'>
                                                     {day.date}
                                                 </div>
+                                                {
+                                                    day.dayEvents.map(event => {
+                                                        return (
+                                                            <div key={event.id}>
+                                                                <div className='calendar-event' style={{ backgroundColor: event.color }}>
+                                                                    {event.name}
+                                                                </div>
+                                                            </div>
+                                                        );
+                                                    })
+                                                }
                                             </div>
                                         );
                                     })}
